c4cbfc6552dcb7615182e810e47235b86b3e92c1
willianzocolau <willianzocolau@gmail.com>
2021-06-16 21:27:54 -0300

    add bundle start

---
+++ lib/parallelism.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "parallelism/version"
+
+module Parallelism
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/parallelism/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Parallelism
+  VERSION = "0.1.0"
+end
